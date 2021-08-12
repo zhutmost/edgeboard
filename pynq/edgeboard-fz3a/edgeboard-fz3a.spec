6b0407dbb977b9c7c1f509af26783928f2ff2684
zhutmost <zhutmost@outlook.com>
2021-08-13 00:11:42 +0800

    feat: add necessary files for PYNQ compilation

---
+++ pynq/edgeboard-fz3a/edgeboard-fz3a.spec
@@ -0,0 +1,6 @@
+ARCH_edgeboard-fz3a := aarch64
+BSP_edgeboard-fz3a :=
+BITSTREAM_edgeboard-fz3a := base/base.bit
+FPGA_MANAGER_edgeboard-fz3a := 1
+
+STAGE4_PACKAGES_edgeboard-fz3a := pynq ethernet boot_leds
---
+++ pynq/edgeboard-fz3a/packages/boot_leds/boot.py
@@ -0,0 +1,13 @@
+#!/bin/env python3.6
+
+from pynq import Overlay
+from time import sleep
+
+ol = Overlay('base.bit')
+led = ol.gpio_led.channel1
+
+for _ in range(8):
+    led[0].on()
+    sleep(0.2)
+    led[0].off()
+    sleep(0.2)
